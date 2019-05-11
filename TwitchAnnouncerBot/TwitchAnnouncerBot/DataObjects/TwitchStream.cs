cf07fb9599dd0b7c879876cb0d82d63c0e44be17
Jedimaster4559 <nbsolomon4559@gmail.com>
2019-05-10 21:05:41 -0500

    Created initial object. Will expand with more research into twitch api

---
+++ TwitchAnnouncerBot/TwitchAnnouncerBot/DataObjects/TwitchStream.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TwitchAnnouncerBot.DataObjects
+{
+    public class TwitchStream
+    {
+        string name;
+
+        public TwitchStream(string name)
+        {
+            this.name = name;
+        }
+    }
+}
