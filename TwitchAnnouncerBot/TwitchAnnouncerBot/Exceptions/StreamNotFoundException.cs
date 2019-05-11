34859e9454f7d7eee4c56ab08ef788530145e11e
Jedimaster4559 <nbsolomon4559@gmail.com>
2019-05-10 21:25:35 -0500

    added basic exceptions for some common errors that may happen

---
+++ TwitchAnnouncerBot/TwitchAnnouncerBot/Exceptions/StreamNotFoundException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace TwitchAnnouncerBot.DataObjects
+{
+    [Serializable]
+    internal class StreamNotFoundException : Exception
+    {
+        public StreamNotFoundException()
+        {
+        }
+
+        public StreamNotFoundException(string message) : base(message)
+        {
+        }
+
+        public StreamNotFoundException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected StreamNotFoundException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
