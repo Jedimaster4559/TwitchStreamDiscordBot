0b02f83308d735b5bbe440e5ffebf55649189ed9
Jedimaster4559 <nbsolomon4559@gmail.com>
2019-05-11 22:16:52 -0500

    started beginnnings  of subscribe command

---
+++ TwitchAnnouncerBot/TwitchAnnouncerBot/Commands/Subscribe.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+
+namespace TwitchAnnouncerBot
+{
+    public class Subscribe : ModuleBase<SocketCommandContext>
+    {
+        [Command("Subscribe")]
+        [Alias("subscribe","sub","Sub")]
+        public async Task subscribe([Remainder] string twitchChannel)
+        {
+
+        }
+    }
+}
